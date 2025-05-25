d93a53d35fd90afd72abae6f2a4efd5dbda0cab0
Fatima <fatima_97_06@hotmail.com>
2025-05-25 14:42:19 +0200

    Data-laag toegevoegd

---
+++ Data/BestelLijnenDAO.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Data;
+
+use \PDO;
+use Data\DBConfig;
+use Entities\BestelLijn;
+
+class BestelLijnenDAO
+{
+    public function connect(): PDO
+    {
+        return new PDO(
+            DBConfig::$DB_CONNSTRING,
+            DBConfig::$DB_USERNAME,
+            DBConfig::$DB_PASSWORD
+        );
+    }
+
+    public function getAll() {}
+
+    public function getById() {}
+
+    public function create() {}
+
+    public function update() {}
+
+    public function delete() {}
+}
---
+++ Data/DBConfig.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Data;
+
+class DBConfig
+{
+    public static $DB_CONNSTRING = "mysql:host=localhost;port=3307;dbname=pizzabar_eindoefening;charset=utf8";
+    public static $DB_USERNAME = "root";
+    public static $DB_PASSWORD = "";
+}

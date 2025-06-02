18145ee6b56933118ab5b38e4e7ce62ad5cca1d2
Fatima <fatima_97_06@hotmail.com>
2025-06-02 15:59:08 +0200

    begin presentation

---
+++ Presentation/header.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gebruikerstoegang</title>
+    <style>
+        .menu-container {
+            padding: 1em;
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+            gap: 1em;
+        }
+
+        .afbeelding-container img {
+            width: 160px;
+            height: 120px;
+        }
+
+        .omschrijving-container {
+            margin: 1em;
+            text-align: left;
+        }
+    </style>
+</head>
+
+<body>
+    <header>
+        <h1>Fabeki Pizzeria</h1>
+        <nav>
+            <a href="indexPizzas.php">Overzicht pizza's</a>
+            <?php if (!isset($_SESSION["gebruiker"])) { ?>
+                <a href="loginForm.php">Login</a>
+                <a href="registerForm.php">Registreer</a>
+            <?php } else { ?>
+                <a href="overzichtBestelling.php">Mijn bestelling</a>
+            <?php } ?>
+            <a href="algemeenInfo.php">Algemeen info</a>
+        </nav>
+    </header>

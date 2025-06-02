7abda19386ee7ccaaed96f18bd8dacf0e59a458a
Fatima <fatima_97_06@hotmail.com>
2025-06-02 15:27:07 +0200

    Data-pizzas-getAll

--- Data/PizzasDAO.php
+++ Data/PizzasDAO.php
@@ -7,6 +7,7 @@ namespace Data;
 use \PDO;
 use Data\DBConfig;
 use Entities\Pizza;
+use Entities\Voedingswaarde;
 
 class PizzasDAO
 {
@@ -17,5 +18,40 @@ class PizzasDAO
             DBConfig::$DB_USERNAME,
             DBConfig::$DB_PASSWORD
         );
+    }
+
+    public function getAll(): array
+    {
+        $sql = "SELECT Pizzas.*, V.energie, V.vet, V.koolhydraat, V.eiwit FROM Pizzas
+                INNER JOIN Voedingswaarden V ON V.voedingswaardeId = Pizzas.voedingswaarde_id";
+        $dbh = $this->connect();
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
+        $resultSet = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $pizzas = [];
+        if (!empty($resultSet)) {
+            foreach ($resultSet as $row) {
+
+                $voedingswaarde = new Voedingswaarde(
+                    (int)$row["voedingswaardeId"],
+                    (float)$row["energie"],
+                    (float)$row["vet"],
+                    (float)$row["koolhydraat"],
+                    (float)$row["eiwit"]
+                );
+
+                $pizza = new Pizza(
+                    (int)$row["pizzaId"],
+                    $row["pizzaNaam"],
+                    (float)$row["prijs"],
+                    $row["samenstelling"],
+                    $voedingswaarde,
+                    (bool)$row["beschikbaarheid"],
+                    (bool)$row["promo"]
+                );
+                $pizzas[] = $pizza;
+            }
+        }
+        return $pizzas;
     }
 }

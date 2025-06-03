c85fe133311e4868142bbc922b4c426394b934d5
Fatima <fatima_97_06@hotmail.com>
2025-06-03 13:28:15 +0200

    presentation overzicht pizzas

---
+++ Presentation/indexPizzas.php
@@ -0,0 +1,32 @@
+<?php require_once("header.php"); ?>
+
+<h2>Aanbod Pizza's</h2>
+
+<?php if (empty($pizzaLijst)) { ?>
+    <p>Geen pizza's gevonden</p>
+    <?php } else {
+    foreach ($pizzaLijst as $pizza) : ?>
+        <div class="product-container">
+
+            <div class="afbeelding-container">
+                <img src="<?= $pizza->getPizzaNaam(); ?>.avif" alt="<?= $pizza->getPizzaNaam(); ?>">
+            </div>
+
+            <div class="omschrijving-container">
+                <p><?= $pizza->getPizzaNaam(); ?></p>
+                <p>€<?= $pizza->getPrijs(); ?></p>
+                <p><?= $pizza->getSamenstelling(); ?></p>
+                <div class="voedingswaarden">
+                    <p>Energie: <?= $pizza->getVoedingswaarde()->getEnergie(); ?> Kcal</p>
+                    <p>Vetten: <?= $pizza->getVoedingswaarde()->getVet(); ?> g</p>
+                    <p>Koolhydraten: <?= $pizza->getVoedingswaarde()->getKoolhydraat(); ?> g</p>
+                    <p>Eiwitten: <?= $pizza->getVoedingswaarde()->getEiwit(); ?> g</p>
+                </div>
+
+            </div>
+
+        </div>
+<?php endforeach;
+} ?>
+
+<?php require_once("footer.php"); ?>

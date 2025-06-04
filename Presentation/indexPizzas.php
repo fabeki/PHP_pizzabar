c7b853baf89954793bf5260d4ac4adb57e61d416
Fatima <fatima_97_06@hotmail.com>
2025-06-05 01:19:17 +0200

    1ste pagina wordt correct getoond met mandje

--- Presentation/indexPizzas.php
+++ Presentation/indexPizzas.php
@@ -4,29 +4,83 @@
 
 <?php if (empty($pizzaLijst)) { ?>
     <p>Geen pizza's gevonden</p>
-    <?php } else {
-    foreach ($pizzaLijst as $pizza) : ?>
-        <div class="product-container">
+<?php } else { ?>
+    <div class="menu-container">
 
-            <div class="afbeelding-container">
-                <img src="img/<?= $pizza->getPizzaNaam(); ?>.avif" alt="<?= $pizza->getPizzaNaam(); ?>">
+        <?php foreach ($pizzaLijst as $pizza) : ?>
+
+            <div class="product-container">
+
+
+                <form method="post" action="ctrlIndexPizzas.php">
+
+                    <div class="afbeelding-omschrijving-container">
+
+                        <div class="afbeelding-container">
+                            <img src="img/<?= $pizza->getPizzaNaam(); ?>.avif" alt="<?= $pizza->getPizzaNaam(); ?>">
+                        </div>
+
+                        <div class="omschrijving-container">
+                            <input type="hidden" name="pizzaId" value="<?= $pizza->getPizzaId(); ?>">
+                            <p><?= $pizza->getPizzaNaam(); ?></p>
+                            <p>€<?= $pizza->getPrijs(); ?></p>
+                            <p><?= $pizza->getSamenstelling(); ?></p>
+                            <div class="voedingswaarden">
+                                <p>Energie: <?= $pizza->getVoedingswaarde()->getEnergie(); ?> Kcal</p>
+                                <p>Vetten: <?= $pizza->getVoedingswaarde()->getVet(); ?> g</p>
+                                <p>Koolhydraten: <?= $pizza->getVoedingswaarde()->getKoolhydraat(); ?> g</p>
+                                <p>Eiwitten: <?= $pizza->getVoedingswaarde()->getEiwit(); ?> g</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div class="button-container">
+                        <input type="submit" value="Toevoegen">
+                    </div>
+
+                </form>
+
             </div>
 
-            <div class="omschrijving-container">
-                <p><?= $pizza->getPizzaNaam(); ?></p>
-                <p>€<?= $pizza->getPrijs(); ?></p>
-                <p><?= $pizza->getSamenstelling(); ?></p>
-                <div class="voedingswaarden">
-                    <p>Energie: <?= $pizza->getVoedingswaarde()->getEnergie(); ?> Kcal</p>
-                    <p>Vetten: <?= $pizza->getVoedingswaarde()->getVet(); ?> g</p>
-                    <p>Koolhydraten: <?= $pizza->getVoedingswaarde()->getKoolhydraat(); ?> g</p>
-                    <p>Eiwitten: <?= $pizza->getVoedingswaarde()->getEiwit(); ?> g</p>
-                </div>
+        <?php endforeach; ?>
 
-            </div>
+        <section class="mand-container">
+            <form action="ctrlMandjeAfrekenen.php" method="post">
+                <header>
+                    <h3>Bestelling</h3>
+                </header>
 
-        </div>
-<?php endforeach;
-} ?>
+                <?php $subTot = 0;
+                $tot = 0;
+                if (isset($_SESSION["mandje"]) && !empty($_SESSION["mandje"])): ?>
+                    <div class="bestelling-container">
+                        <?php foreach ($_SESSION["mandje"] as $pizzaId => $aantal): // '=>' sleutels/waarden uit array halen
+                            foreach ($pizzaLijst as $pizza):
+                                if ($pizza->getPizzaId() === (int)$pizzaId):
+                                    $subTot = $pizza->getPrijs() * $aantal;
+                                    $tot += $subTot;
+                        ?>
+                                    <div>
+                                        <div> <?= $aantal ?>x <?= $pizza->getPizzaNaam(); ?> €<?= number_format($subTot, 2); ?></div>
+                                        <div><a href="ctrlIndexPizzas.php?remove=<?= $pizzaId ?>">Verwijder</a></div>
+                                    </div>
+                        <?php endif;
+                            endforeach;
+                        endforeach; ?>
 
-<?php require_once("footer.php"); ?>
+                        <div>Totaal €<?= number_format($tot, 2); ?></div>
+                        <div><input type="submit" value="Afrekenen"></div>
+                    </div>
+
+                <?php else: ?>
+                    <div>Mandje is leeg, kies een pizza.</div>
+                <?php endif; ?>
+            </form>
+        </section>
+
+
+    <?php } ?>
+
+    <?php require_once("footer.php"); ?>

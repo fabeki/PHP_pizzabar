77f27a78f1a1c14c87355098008e29a1b2580c0d
Fatima <fatima_97_06@hotmail.com>
2025-06-08 01:02:31 +0200

    plaatsendao aangemaakt

--- Data/PlaatsenDAO.php
+++ Data/PlaatsenDAO.php
@@ -18,4 +18,36 @@ class PlaatsenDAO
             DBConfig::$DB_PASSWORD
         );
     }
+    // gebruik functies later voor controle om dubbele records te vermijden
+    public function getByWoonplaats(string $woonplaats): ?Plaats
+    {
+        $sql = "SELECT * FROM plaatsen WHERE woonplaats=:woonplaats";
+        $dbh = $this->connect();
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute([':woonplaats' => $woonplaats]);
+        $record = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if (empty($record)) return null;
+
+        return new Plaats(
+            (int)$record['id'],
+            (int)$record['postcode'],
+            $record['woonplaats']
+        );
+    }
+
+    public function create(int $postcode, string $woonplaats): int
+    {
+        $sql = "INSERT INTO plaatsen (postcode, woonplaats)
+                VALUES (:postcode,:woonplaats)";
+        $dbh = $this->connect();
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute(
+            [
+                ':postcode' => $postcode,
+                ':woonplaats' => $woonplaats
+            ]
+        );
+        return (int)$dbh->lastInsertId();
+    }
 }

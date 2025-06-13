fba2467ea7f246461e01fd09c987eff214e3797e
Fatima <fatima_97_06@hotmail.com>
2025-06-13 13:57:58 +0200

    algemeeninfo toegevoegd

---
+++ Presentation/algemeenInfo.php
@@ -0,0 +1,26 @@
+<?php require_once("header.php"); ?>
+
+<h2>Wie zijn wij?</h2>
+
+<section>
+    <p>Welkom Bij Pizzabar FaBeKi</p>
+    <div>
+        <p>Adres: Nieuwendijk 81, 3582 Koersel</p>
+        <p>Contact: info@pizzabarfabeki.com
+            / +32458236159</p>
+        <p>Openingstijden: Wo-Zo 15.00-01.00, Ma-Di gesloten.</p>
+    </div>
+</section>
+<section>
+    <h2>Lopende promoties</h2>
+    <ul>
+        <li>...</li>
+        <li>...</li>
+    </ul>
+</section>
+<section>
+    <h2>Algemene voorwaarden</h2>
+    <p>Betaling kan contant of via bankkaart/payconiq/crypto.</p>
+</section>
+
+<?php require_once("footer.php"); ?>

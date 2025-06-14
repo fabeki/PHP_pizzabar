6850ee217c31ef8fdad433020d20e7d09e5dec2f
Fatima <fatima_97_06@hotmail.com>
2025-06-14 22:17:15 +0200

    ctrl voor overzicht bestelling

---
+++ ctrlOverzichtBestelling.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+spl_autoload_register();
+
+session_start();
+
+use Business\PizzaService;
+use Business\PlaatsService;
+
+//overzicht bestelling
+$pizzaSVC = new PizzaService();
+$tot = 0;
+$mandje = [];
+
+if (isset($_SESSION["mandje"])) {
+    foreach ($_SESSION["mandje"] as $pizzaId => $aantal) {
+        $pizza = $pizzaSVC->getPizzaById((int)$pizzaId);
+        $prijs = $pizza->getPrijs();
+        $subTot = $prijs * $aantal;
+        $tot += $subTot;
+
+        $mandje[] = [
+            'naam' => $pizza->getNaam(),
+            'aantal' => $aantal,
+            'prijs' => $prijs,
+            'subTot' => $subTot
+        ];
+    }
+}
+
+//leveringplaats 
+$plaatsSVC = new PlaatsService();
+$leveringsPlaatsen = $plaatsSVC->getPlaatsenByPostcodes([3580, 3581, 3582]);
+
+//tijd
+$tijd = new DateTime();
+$tijd->add(new DateInterval('PT30M'));
+$minTijd = $tijd->format('Y-m-d\TH:i');
+
+include("Presentation/overzichtBestelling.php");

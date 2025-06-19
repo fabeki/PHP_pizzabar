73ef43ad33cb6ec81ae7ed865d9998385f262364
Fatima <fatima_97_06@hotmail.com>
2025-06-19 14:57:39 +0200

    catch in ctrllogin

--- ctrlLogin.php
+++ ctrlLogin.php
@@ -7,21 +7,26 @@ session_start();
 spl_autoload_register();
 
 use Business\KlantService;
+use Exceptions\GebruikerBestaatNietException;
+use Exceptions\WachtwoordIncorrectException;
+
+$error = "";
 
 if (isset($_POST["btnLogin"])) {
     $email = $_POST["txtEmail"];
     $ww = $_POST["txtWachtwoord"];
 
     $klantSVC = new KlantService();
-    $klant = $klantSVC->controleerLogin($email, $ww);
 
-    if ($klant === null) {
-        include("/Pizzabar/ctrlLogin.php");
-        exit();
-    } else {
+    try {
+        $klant = $klantSVC->controleerLogin($email, $ww);
         $_SESSION["gebruiker"] = serialize($klant);
         header("Location: /Pizzabar/ctrlOverzichtBestelling.php");
         exit(0);
+    } catch (GebruikerBestaatNietException $e) {
+        $error .= "E-mailadres bestaat niet.";
+    } catch (WachtwoordIncorrectException $e) {
+        $error .= "Wachtwoord incorrect.";
     }
 }
 

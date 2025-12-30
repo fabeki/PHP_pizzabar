1bc3cc760510fcbe94cd66d488bf29646f75c9cf
Fatima <fatima_97_06@hotmail.com>
2025-12-30 01:36:22 +0100

    DB port 3306

--- Data/DBConfig.php
+++ Data/DBConfig.php
@@ -6,7 +6,7 @@ namespace Data;
 
 class DBConfig
 {
-    public static $DB_CONNSTRING = "mysql:host=localhost;port=3307;dbname=pizzabar_eindoefening;charset=utf8";
+    public static $DB_CONNSTRING = "mysql:host=localhost;port=3306;dbname=pizzabar_eindoefening;charset=utf8";
     public static $DB_USERNAME = "root";
     public static $DB_PASSWORD = "";
 }
